56b33d18a1ba46c4c01647880b92f3b6d830d22a
HayataToshihiro <koda123456789koda@yahoo.co.jp>
2019-04-19 18:23:29 +0900

    commit

--- src/making_localmap.cpp
+++ src/making_localmap.cpp
@@ -30,15 +30,13 @@ class OccupancyGridLidar{
 		const double w = 20.0;	//x[m]
 		const double h = 20.0;	//y[m]
 		const double resolution = 0.2;	//[m]
-		const double threshold_curvature = 3.0e-4;
-		const double range_road_intensity[2] = {5, 15};
+		// const double range_road_intensity[2] = {5, 15};
 	public:
 		OccupancyGridLidar();
 		void GridInitialization(void);
 		void CallbackRmGround(const sensor_msgs::PointCloud2ConstPtr& msg);
 		void CallbackGround(const sensor_msgs::PointCloud2ConstPtr& msg);
 		void ExtractPCInRange(pcl::PointCloud<pcl::PointXYZI>::Ptr pc);
-		void NormalEstimation(void);
 		void InputGrid(void);
 		int MeterpointToIndex(double x, double y);
 		void Publication(void);
@@ -47,13 +45,13 @@ class OccupancyGridLidar{
 
 OccupancyGridLidar::OccupancyGridLidar()
 {
-	sub_rmground = nh.subscribe("/rm_ground2/renamed_frame/transformed", 1, &OccupancyGridLidar::CallbackRmGround, this);
-	sub_ground = nh.subscribe("/ground2/renamed_frame/transformed", 1, &OccupancyGridLidar::CallbackGround, this);
+	sub_rmground = nh.subscribe("/rm_ground", 1, &OccupancyGridLidar::CallbackRmGround, this);
+	sub_ground = nh.subscribe("/ground", 1, &OccupancyGridLidar::CallbackGround, this);
 	pub = nh.advertise<nav_msgs::OccupancyGrid>("/occupancygrid/lidar", 1);
 	GridInitialization();
 }
 
-void OccupancyGridLidar::GridInitialization(void)
+void OccupancyGridLidar::GridInitialization(void)/*{{{*/
 {
 	grid.info.resolution = resolution;
 	grid.info.width = w/resolution + 1;
@@ -67,11 +65,10 @@ void OccupancyGridLidar::GridInitialization(void)
 	grid.info.origin.orientation.w = 1.0;
 	for(int i=0;i<grid.info.width*grid.info.height;i++)	grid.data.push_back(-1);
 	// frame_id is same as the one of subscribed pc
-	
 	grid_all_minusone = grid;
-}
+}/*}}}*/
 
-void OccupancyGridLidar::CallbackRmGround(const sensor_msgs::PointCloud2ConstPtr &msg)
+void OccupancyGridLidar::CallbackRmGround(const sensor_msgs::PointCloud2ConstPtr &msg)/*{{{*/
 {
 	pcl::fromROSMsg(*msg, *rmground);
 
@@ -82,9 +79,9 @@ void OccupancyGridLidar::CallbackRmGround(const sensor_msgs::PointCloud2ConstPtr
 
 	InputGrid();
 	Publication();
-}
+}/*}}}*/
 
-void OccupancyGridLidar::CallbackGround(const sensor_msgs::PointCloud2ConstPtr &msg)
+void OccupancyGridLidar::CallbackGround(const sensor_msgs::PointCloud2ConstPtr &msg)/*{{{*/
 {
 	pcl::PointCloud<pcl::PointXYZI>::Ptr tmp_pc {new pcl::PointCloud<pcl::PointXYZI>};
 	pcl::fromROSMsg(*msg, *tmp_pc);
@@ -92,11 +89,9 @@ void OccupancyGridLidar::CallbackGround(const sensor_msgs::PointCloud2ConstPtr &
 	ExtractPCInRange(tmp_pc);
 	
 	pcl::copyPointCloud(*tmp_pc, *ground);
+}/*}}}*/
 
-	NormalEstimation();
-}
-
-void OccupancyGridLidar::ExtractPCInRange(pcl::PointCloud<pcl::PointXYZI>::Ptr pc)
+void OccupancyGridLidar::ExtractPCInRange(pcl::PointCloud<pcl::PointXYZI>::Ptr pc)/*{{{*/
 {
 	pcl::PassThrough<pcl::PointXYZI> pass;
 	pass.setInputCloud(pc);
@@ -107,29 +102,19 @@ void OccupancyGridLidar::ExtractPCInRange(pcl::PointCloud<pcl::PointXYZI>::Ptr p
 	pass.setFilterFieldName("y");
 	pass.setFilterLimits(-h/2.0, h/2.0);
 	pass.filter(*pc);
-}
-
-void OccupancyGridLidar::NormalEstimation(void)
-{
-	pcl::NormalEstimation<pcl::PointXYZINormal, pcl::PointXYZINormal> ne;
-	ne.setInputCloud (ground);
-	pcl::search::KdTree<pcl::PointXYZINormal>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZINormal> ());
-	ne.setSearchMethod(tree);
-	ne.setRadiusSearch(0.1);
-	ne.compute(*ground);
-}
+}/*}}}*/
 
 void OccupancyGridLidar::InputGrid(void)
 {
 	grid = grid_all_minusone;
-
+	//intensity
 	for(size_t i=0;i<ground->points.size();i++){
-		// std::cout << "ground->points[i].curvature = " << ground->points[i].curvature << std::endl;
-		// std::cout << "ground->points[i].intensity = " << ground->points[i].intensity << std::endl;
-		if(ground->points[i].curvature>threshold_curvature)	grid.data[MeterpointToIndex(ground->points[i].x, ground->points[i].y)] = 50;
-		else if(ground->points[i].intensity<range_road_intensity[0] || ground->points[i].intensity>range_road_intensity[1])	grid.data[MeterpointToIndex(ground->points[i].x, ground->points[i].y)] = 50;
-		else	grid.data[MeterpointToIndex(ground->points[i].x, ground->points[i].y)] = 0;
+		// if(ground->points[i].intensity<range_road_intensity[0] || ground->points[i].intensity>range_road_intensity[1])	grid.data[MeterpointToIndex(ground->points[i].x, ground->points[i].y)] = 50;
+		// else	grid.data[MeterpointToIndex(ground->points[i].x, ground->points[i].y)] = 0;
+		
+		grid.data[MeterpointToIndex(ground->points[i].x, ground->points[i].y)] = 0;
 	}
+	//obstacle
 	for(size_t i=0;i<rmground->points.size();i++){
 		grid.data[MeterpointToIndex(rmground->points[i].x, rmground->points[i].y)] = 100;
 	}
