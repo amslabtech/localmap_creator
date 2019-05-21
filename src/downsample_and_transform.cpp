d52e3705366da87fee10347150aed30a9c5601a4
HayataToshihiro <koda123456789koda@yahoo.co.jp>
2019-05-21 19:39:15 +0900

    commit

--- src/downsample_and_transform.cpp
+++ src/downsample_and_transform.cpp
@@ -1,6 +1,14 @@
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/point_cloud_conversion.h>
+#include <pcl_ros/point_cloud.h>
+#include <pcl_ros/transforms.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <pcl_conversions/pcl_conversions.h>
 #include <tf/transform_listener.h>
 
 class PointCloudTransform{
@@ -11,7 +19,6 @@ class PointCloudTransform{
 		ros::Subscriber sub;
 		ros::Publisher pub;
 		sensor_msgs::PointCloud pc_;
-		std::string target;
 
 	public:
 		PointCloudTransform();
@@ -22,21 +29,30 @@ PointCloudTransform::PointCloudTransform()
     : nhPrivate("~")
 {
 	sub = nh.subscribe("/cloud", 1, &PointCloudTransform::Callback, this);
-	pub = nh.advertise<sensor_msgs::PointCloud2>("/cloud/transformed", 1);
-	nhPrivate.getParam("target", target);
+	pub = nh.advertise<sensor_msgs::PointCloud2>("/cloud/downsampled", 1);
 }
 
 void PointCloudTransform::Callback(const sensor_msgs::PointCloud2ConstPtr &msg)
 {
-	sensor_msgs::PointCloud pc_in;
-	sensor_msgs::PointCloud pc_trans;
+	sensor_msgs::PointCloud2 pc2_trans;
 	sensor_msgs::PointCloud2 pc2_out;
-	sensor_msgs::convertPointCloud2ToPointCloud(*msg, pc_in);
-
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr pcl_cloud (new pcl::PointCloud<pcl::PointXYZRGB>());
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr ds_cloud {new pcl::PointCloud<pcl::PointXYZRGB>()};
+    ros::Time time = ros::Time(0);
 	try{
-		tflistener.waitForTransform(target, msg->header.frame_id, msg->header.stamp, ros::Duration(1.0));
-		tflistener.transformPointCloud(target, msg->header.stamp, pc_in, msg->header.frame_id, pc_trans);
-		sensor_msgs::convertPointCloudToPointCloud2(pc_trans, pc2_out);
+		
+        tflistener.waitForTransform("/base_link", msg->header.frame_id, time, ros::Duration(4.0));
+		pcl_ros::transformPointCloud("/base_link", *msg, pc2_trans, tflistener);
+		pcl::fromROSMsg(pc2_trans, *pcl_cloud);
+
+		pcl::VoxelGrid<pcl::PointXYZRGB> vg;
+    	vg.setInputCloud (pcl_cloud);  
+    	vg.setLeafSize (0.05, 0.05f, 0.05f);
+    	vg.filter (*ds_cloud);
+
+    	pcl::toROSMsg(*ds_cloud, pc2_out);
+		pc2_out.header.frame_id = "/base_link";
+		pc2_out.header.stamp = msg->header.stamp;
 		pub.publish(pc2_out);
 	}
 	catch(tf::TransformException ex){
@@ -46,8 +62,9 @@ void PointCloudTransform::Callback(const sensor_msgs::PointCloud2ConstPtr &msg)
 
 int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "pc_transform");
-	
+    ros::init(argc, argv, "downsample_and_transform");
 	PointCloudTransform transform;
 	ros::spin();
+	
+	return 0;
 }
