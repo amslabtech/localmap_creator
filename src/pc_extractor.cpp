5ccb08a0c9ac702e99185cdacf58d0b2c65cf078
Toshiki Nakamura <ee193014@meiji.ac.jp>
2023-10-11 01:14:44 +0900

    Fix

--- src/pc_extractor.cpp
+++ src/pc_extractor.cpp
@@ -1,4 +1,3 @@
-#include <boost/mpl/min_max.hpp>
 #include <pcl_ros/point_cloud.h>
 #include <pcl_ros/transforms.h>
 #include <ros/ros.h>
@@ -18,53 +17,54 @@ protected:
     typedef pcl::PointCloud<PointType>::Ptr PointCloudTypePtr;
 
     void cloud_callback(const sensor_msgs::PointCloud2ConstPtr &msg);
-    void extract_points(PointCloudType &cloud);
+    void extract_points(const PointCloudType &cloud);
 
     double max_height_;
     double min_height_;
     std::string target_frame_;
-
     ros::NodeHandle nh_;
     ros::NodeHandle private_nh_;
     ros::Subscriber point_cloud_sub_;
     ros::Publisher point_cloud_pub_;
+    PointCloudType cloud_;
+    tf::TransformListener tfListener_;
 };
 
 PcExtractor::PcExtractor() : private_nh_("~")
 {
+    private_nh_.param<std::string>("target_frame", target_frame_, {"base_link"});
     private_nh_.param("max_height", max_height_, 1.5);
     private_nh_.param("min_height", min_height_, 0.05);
 
     ROS_INFO("=== PC Extractor ===");
+    ROS_INFO_STREAM("target_frame: " << target_frame_);
     ROS_INFO_STREAM("max_height: " << max_height_);
     ROS_INFO_STREAM("min_height: " << min_height_);
 
     point_cloud_sub_ = nh_.subscribe(
         "/cloud_in", 1, &PcExtractor::cloud_callback, this, ros::TransportHints().reliable().tcpNoDelay());
     point_cloud_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("/cloud_out", 1);
+
+    cloud_.header.frame_id = target_frame_;
 }
 
 void PcExtractor::cloud_callback(const sensor_msgs::PointCloud2ConstPtr &msg)
 {
-    PointCloudType pc;
-    pcl::fromROSMsg(*msg, pc);
-    extract_points(pc);
-    point_cloud_pub_.publish(pc);
+    PointCloudTypePtr pc_ptr(new PointCloudType);
+    pcl::fromROSMsg(*msg, *pc_ptr);
+    PointCloudTypePtr pc_transformed_ptr(new PointCloudType);
+    pcl_ros::transformPointCloud(target_frame_, *pc_ptr, *pc_transformed_ptr, tfListener_);
+    extract_points(*pc_transformed_ptr);
+    cloud_.header.stamp = pc_transformed_ptr->header.stamp;
+    point_cloud_pub_.publish(cloud_);
+    cloud_.points.clear();
 }
 
-void PcExtractor::extract_points(PointCloudType &cloud)
+void PcExtractor::extract_points(const PointCloudType &cloud)
 {
-    ROS_WARN_STREAM("cloud size: " << cloud.points.size());
     for (int i = 0; i < cloud.points.size(); i++)
-    {
-        ROS_WARN_STREAM("cloud z: " << cloud.points[i].z);
-        ROS_WARN_STREAM("judege: " << (cloud.points[i].z < min_height_ || max_height_ < cloud.points[i].z));
-        if (cloud.points[i].z < min_height_ || max_height_ < cloud.points[i].z)
-            cloud.points.erase(cloud.points.begin() + i);
-    }
-    ROS_WARN("extract");
-    ROS_WARN_STREAM("cloud size: " << cloud.points.size());
-    ROS_WARN("---");
+        if (min_height_ < cloud.points[i].z && cloud.points[i].z < max_height_)
+            cloud_.points.push_back(cloud.points[i]);
 }
 
 void PcExtractor::process() { ros::spin(); }
