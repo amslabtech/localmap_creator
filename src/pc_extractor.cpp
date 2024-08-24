f0889899126c52c0641c0862c178cd98023306be
Toshiki Nakamura <82020865+ToshikiNakamura0412@users.noreply.github.com>
2024-08-24 18:32:47 +0900

    Refactoring pc extractor (#32)
    * Fix format
    
    * Use passthrough
    
    * Refactoring

--- src/pc_extractor.cpp
+++ src/pc_extractor.cpp
@@ -1,3 +1,4 @@
+#include <pcl/filters/passthrough.h>
 #include <pcl_ros/point_cloud.h>
 #include <pcl_ros/transforms.h>
 #include <ros/ros.h>
@@ -8,72 +9,75 @@
 class PcExtractor
 {
 public:
-    PcExtractor();
-    void process();
+  PcExtractor(void);
 
 protected:
-    typedef pcl::PointXYZ PointType;
-    typedef pcl::PointCloud<PointType> PointCloudType;
-    typedef pcl::PointCloud<PointType>::Ptr PointCloudTypePtr;
+  typedef pcl::PointXYZ PointT;
+  typedef pcl::PointCloud<PointT> PointCloudT;
 
-    void cloud_callback(const sensor_msgs::PointCloud2ConstPtr &msg);
-    void extract_points(const PointCloudType &cloud);
+  void cloud_callback(const sensor_msgs::PointCloud2ConstPtr &msg);
+  void passthrough_filter(PointCloudT::Ptr cloud, const std::string &axis, const float min, const float max);
 
-    double max_height_;
-    double min_height_;
-    std::string target_frame_;
-    ros::NodeHandle nh_;
-    ros::NodeHandle private_nh_;
-    ros::Subscriber point_cloud_sub_;
-    ros::Publisher point_cloud_pub_;
-    PointCloudType cloud_;
-    tf::TransformListener tfListener_;
+  std::string target_frame_;
+  float max_height_;
+  float min_height_;
+  ros::NodeHandle nh_;
+  ros::NodeHandle private_nh_;
+  ros::Subscriber point_cloud_sub_;
+  ros::Publisher point_cloud_pub_;
+  tf::TransformListener tf_listener_;
 };
 
-PcExtractor::PcExtractor() : private_nh_("~")
+PcExtractor::PcExtractor(void) : private_nh_("~")
 {
-    private_nh_.param<std::string>("target_frame", target_frame_, {"base_link"});
-    private_nh_.param("max_height", max_height_, 1.5);
-    private_nh_.param("min_height", min_height_, 0.05);
+  private_nh_.param<std::string>("target_frame", target_frame_, {"base_link"});
+  private_nh_.param<float>("max_height", max_height_, 1.5);
+  private_nh_.param<float>("min_height", min_height_, 0.05);
 
-    ROS_INFO("=== PC Extractor ===");
-    ROS_INFO_STREAM("target_frame: " << target_frame_);
-    ROS_INFO_STREAM("max_height: " << max_height_);
-    ROS_INFO_STREAM("min_height: " << min_height_);
+  ROS_INFO("=== PC Extractor ===");
+  ROS_INFO_STREAM("target_frame: " << target_frame_);
+  ROS_INFO_STREAM("max_height: " << max_height_);
+  ROS_INFO_STREAM("min_height: " << min_height_);
 
-    point_cloud_sub_ = nh_.subscribe(
-        "/cloud_in", 1, &PcExtractor::cloud_callback, this, ros::TransportHints().reliable().tcpNoDelay());
-    point_cloud_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("/cloud_out", 1);
-
-    cloud_.header.frame_id = target_frame_;
+  point_cloud_sub_ =
+      nh_.subscribe("/cloud_in", 1, &PcExtractor::cloud_callback, this, ros::TransportHints().reliable().tcpNoDelay());
+  point_cloud_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("/cloud_out", 1);
 }
 
 void PcExtractor::cloud_callback(const sensor_msgs::PointCloud2ConstPtr &msg)
 {
-    PointCloudTypePtr pc_ptr(new PointCloudType);
-    pcl::fromROSMsg(*msg, *pc_ptr);
-    PointCloudTypePtr pc_transformed_ptr(new PointCloudType);
-    pcl_ros::transformPointCloud(target_frame_, *pc_ptr, *pc_transformed_ptr, tfListener_);
-    extract_points(*pc_transformed_ptr);
-    cloud_.header.stamp = pc_transformed_ptr->header.stamp;
-    point_cloud_pub_.publish(cloud_);
-    cloud_.points.clear();
+  // convert
+  PointCloudT::Ptr cloud(new PointCloudT);
+  pcl::fromROSMsg(*msg, *cloud);
+
+  // transform
+  pcl_ros::transformPointCloud(target_frame_, *cloud, *cloud, tf_listener_);
+
+  // filter
+  passthrough_filter(cloud, "z", min_height_, max_height_);
+
+  // publish
+  sensor_msgs::PointCloud2 cloud_msg;
+  pcl::toROSMsg(*cloud, cloud_msg);
+  cloud_msg.header.stamp = msg->header.stamp;
+  cloud_msg.header.frame_id = target_frame_;
+  point_cloud_pub_.publish(cloud_msg);
 }
 
-void PcExtractor::extract_points(const PointCloudType &cloud)
+void PcExtractor::passthrough_filter(PointCloudT::Ptr cloud, const std::string &axis, const float min, const float max)
 {
-    for (int i = 0; i < cloud.points.size(); i++)
-        if (min_height_ < cloud.points[i].z && cloud.points[i].z < max_height_)
-            cloud_.points.push_back(cloud.points[i]);
+  pcl::PassThrough<PointT> pass;
+  pass.setInputCloud(cloud);
+  pass.setFilterFieldName(axis);
+  pass.setFilterLimits(min, max);
+  pass.filter(*cloud);
 }
-
-void PcExtractor::process() { ros::spin(); }
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "pc_extractor");
-    PcExtractor pc_extractor;
-    pc_extractor.process();
+  ros::init(argc, argv, "pc_extractor");
+  PcExtractor pc_extractor;
+  ros::spin();
 
-    return 0;
+  return 0;
 }
